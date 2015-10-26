ffdc5a85d6e21aacc74ebf1e7347c30ccfce2204
Luca <luca.bottani@altralogica.it>
2015-10-26 17:45:02 +0100

    no message

--- Sources/dirty2d/Common.hx
+++ Sources/dirty2d/Common.hx
@@ -9,7 +9,6 @@ class Common
 	/** This method checks if the given Xml element is really a Xml element! */
 	public static function isValidElement(element:Xml):Bool {
 		if (element == null) return false;
-		trace(element.nodeType);
 		return Std.string(element.nodeType) == "0";
 	}
 
